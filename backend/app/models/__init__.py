481d243115a409d6031fa137e3032e5fe3859234
rsvidron <rsvidron@gmail.com>
2026-03-07 16:40:54 +0000

    Export TransactionType from models module

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .category import Category
-from .transaction import Transaction
+from .transaction import Transaction, TransactionType
 from .budget import Budget
 from .savings_goal import SavingsGoal
 from .categorization_rule import CategorizationRule
@@ -9,6 +9,7 @@ __all__ = [
     "User",
     "Category",
     "Transaction",
+    "TransactionType",
     "Budget",
     "SavingsGoal",
     "CategorizationRule",
